0b0a82568300a15123d99645589f15c8bb5f296b
Agustin Castro <agus.castro915@gmail.com>
2023-06-28 11:05:08 -0300

    ejercicio 4

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ejercicio 1
+/* 
 int a;
 int b;
 a = 10;
@@ -28,4 +28,71 @@ if (resultado == true)
 else
 {
     Console.WriteLine("El dato ingresado no es un numero");
+}
+ */
+
+// Ejercicio 4
+
+Console.WriteLine("Ingrese un texto: ");
+string? cadena = Console.ReadLine();
+
+Console.WriteLine("longitud: " + cadena.Length);
+
+Console.WriteLine("Ingrese otro texto: ");
+string? cadena2 = Console.ReadLine();
+
+string? concat = string.Concat(cadena, cadena2);
+Console.WriteLine("Concatenacion: " + concat);
+
+foreach (char c in concat)
+{
+    Console.WriteLine(c);
+}
+
+Console.WriteLine(concat.ToUpper());
+Console.WriteLine(concat.ToLower());
+
+
+Console.WriteLine("Ingrese palabra a buscar:");
+string? palabra = Console.ReadLine();
+if (concat.Contains(palabra))
+{
+    Console.WriteLine("SI esta contenida");
+}
+else
+{
+    Console.WriteLine("NO esta contenida");
+}
+
+int a, b;
+Console.WriteLine("Ingrese una ecuacion simple:");
+string? ecuacion = Console.ReadLine();
+
+if (ecuacion.Contains("+"))
+{
+    string[] partes = ecuacion.Split('+');
+    int.TryParse(partes[0], out a);
+    int.TryParse(partes[1], out b);
+    Console.WriteLine("Resultado: " + (a + b));
+}
+else if (ecuacion.Contains("-"))
+{
+    string[] partes = ecuacion.Split('-');
+    int.TryParse(partes[0], out a);
+    int.TryParse(partes[1], out b);
+    Console.WriteLine("Resultado: " + (a - b));
+}
+else if (ecuacion.Contains("*"))
+{
+    string[] partes = ecuacion.Split('*');
+    int.TryParse(partes[0], out a);
+    int.TryParse(partes[1], out b);
+    Console.WriteLine("Resultado: " + (a * b));
+}
+else if (ecuacion.Contains("/"))
+{
+    string[] partes = ecuacion.Split('/');
+    int.TryParse(partes[0], out a);
+    int.TryParse(partes[1], out b);
+    Console.WriteLine("Resultado: " + (a / b));
 }
